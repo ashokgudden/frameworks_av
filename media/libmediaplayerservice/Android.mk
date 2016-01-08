a8c94907b6880a6cabf6f218b062b6287393a87d
Dan Liang <dan.liang@intel.com>
2016-01-08 13:30:51 +0800

    Enable 64bit mediaserver
    Change-Id: I5ee7dc646dfd29436ffef5e391ff8fb25b1325b4
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Signed-off-by: Ivan Pustovit <ivan.pustovit@globallogic.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- media/libmediaplayerservice/Android.mk
+++ media/libmediaplayerservice/Android.mk
@@ -83,7 +83,9 @@ LOCAL_CLANG := true
 
 LOCAL_MODULE:= libmediaplayerservice
 
-#LOCAL_32_BIT_ONLY := true
+ifneq ($(BOARD_USE_64BITMEDIA),true)
+LOCAL_32_BIT_ONLY := true
+endif
 
 include $(BUILD_SHARED_LIBRARY)
 
